4e71f60bbcba756fc4717079a7b5a75320aa65ab
Nikhil Koshatwar <Nikhilk14@users.noreply.huggingface.co>
2026-01-21 12:36:03 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ redis-server --daemonize yes
 
 # 2. Start the Worker (Updated path)
 # We use "-m" to run it as a module from the root folder
-python -m ai_engine.worker &
+python -u -m ai_engine.worker &
 
 # 3. Start the Website
 uvicorn main:app --host 0.0.0.0 --port 7860
